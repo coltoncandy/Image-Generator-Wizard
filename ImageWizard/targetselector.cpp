df982d82290547cf57b74dab0f00dd8a155bb5d5
Tula <tholt@cecs.pdx.edu>
2021-01-12 16:15:48 -0800

    Added a drag box to target, so user can eventually select part of image. Overrided paintEvent, mousePressEvent, mouseMoveEvent, and mouseReleaseEvent.

--- ImageWizard/targetselector.cpp
+++ ImageWizard/targetselector.cpp
@@ -1,6 +1,13 @@
 #include "targetselector.h"
-#include <QLabel>
+//#include <QLabel>
 #include <QImage>
+// Added
+#include <QMouseEvent>
+#include <QAction>
+#include <QFileDialog>
+#include <QPainter>
+#include <QDebug>
+
 
 TargetSelector::TargetSelector(QWidget *parent)
 	: QWidget(parent)
@@ -21,3 +28,87 @@ void TargetSelector::setImage(QImage* image) {
 
 //open up png file as png in C++
 //create mouse events to get coordinates... and give in ratio of total size to png editor in C++
+
+
+void TargetSelector::paintEvent(QPaintEvent* e) {
+	QWidget::paintEvent(e);
+	QPainter painter(this);
+	painter.setPen(QPen(QBrush(QColor(0, 0, 0, 180)), 1, Qt::DashLine));
+	painter.setBrush(QBrush(QColor(255, 255, 255, 120)));
+	painter.drawRect(selectionRect);
+}
+
+void TargetSelector::mousePressEvent(QMouseEvent* e)
+{
+	if (e->button() == Qt::RightButton) {
+		if (selectionRect.contains(e->pos()))
+			contextMenu.exec(this->mapToGlobal(e->pos()));
+	}
+	else {
+		selectionStarted = true;
+		selectionRect.setTopLeft(e->pos());
+		selectionRect.setBottomRight(e->pos());
+	}
+}
+
+void TargetSelector::mouseMoveEvent(QMouseEvent* e)
+{
+	if (selectionStarted) {
+		selectionRect.setBottomRight(e->pos());
+		repaint();
+	}
+}
+
+void TargetSelector::mouseReleaseEvent(QMouseEvent* event)
+{
+	selectionStarted = false;
+}
+
+/*
+void TargetSelector::paintEvent(QPaintEvent* e)
+{
+	//QWidget::paintEvent(e);
+	QPainter painter(this);
+	painter.setPen(QPen(QBrush(QColor(0, 0, 0, 180)), 1, Qt::DashLine));
+	painter.setBrush(QBrush(QColor(255, 255, 255, 120)));
+	painter.drawRect(selectionRect);
+}
+
+
+
+void TargetSelector::mousePressEvent(QMouseEvent *e)
+{
+	if (e->button() == Qt::RightButton)
+	{
+		if (selectionRect.contains(e->pos()))
+			contextMenu.exec(this->mapToGlobal(e->pos()));
+	}
+	else
+	{
+		selectionStarted = true;
+		selectionRect.setTopLeft(e->pos());
+		selectionRect.setBottomRight(e->pos());
+	}
+}
+
+void TargetSelector::mouseMoveEvent(QMouseEvent*e)
+{
+	if (selectionStarted)
+	{
+		selectionRect.setBottomRight(e->pos());
+		repaint();
+	}
+}
+
+void TargetSelector::mouseReleaseEvent(QMouseEvent *e)
+{
+	selectionStarted = false;
+}
+
+
+void TargetSelector::saveSlot()
+{
+	QString fileName = QFileDialog::getSaveFileName(this, QObject::tr("Save File"), "/home", QObject::tr("Images(*.jpg)"));
+	this->pixmap()->copy(selectionRect).save(fileName);
+}
+*/
--- ImageWizard/targetselector.h
+++ ImageWizard/targetselector.h
@@ -3,6 +3,10 @@
 #include <QWidget>
 #include "ui_targetselector.h"
 
+//added
+#include <QLabel>
+#include <QMenu>
+
 class TargetSelector : public QWidget
 {
 	Q_OBJECT
@@ -12,8 +16,37 @@ public:
 	~TargetSelector();
 	void setImage(QImage*);
 
+//added
+protected:
+	
+	void paintEvent(QPaintEvent* e);
+	/*
+	virtual void mousePressEvent(QMouseEvent *e) override;
+	virtual void mouseMoveEvent(QMouseEvent *e) override;
+	virtual void mouseReleaseEvent(QMouseEvent* e) override;
+	*/
+
 private:
 	Ui::targetselector ui;
 	
 	QImage* initialImage;
+
+	//added 
+	QPoint origin;
+
+	QPixmap image;
+	void mousePressEvent(QMouseEvent* event);
+	void mouseMoveEvent(QMouseEvent* event);
+	void mouseReleaseEvent(QMouseEvent* event);
+
+	bool selectionStarted;
+	QRect selectionRect;
+	QMenu contextMenu;
+	
+
+
+	/*
+private slots:
+	void saveSlot();
+	*/
 };

fcbcdbade64a21266e0b7e2cc4a4ecb59e0cd0be
Colton <colton6@pdx.edu>
2021-01-14 18:23:27 -0800

    -test function for static library

--- ImageWizard/main.cpp
+++ ImageWizard/main.cpp
@@ -3,9 +3,13 @@
 
 #include "filechooser.h"
 #include "imagewizard.h"
+#include "../AlgoManager/AlgoManager.cpp"
 
 int main(int argc, char *argv[])
 {
+    //test function for static library... to be deleted
+    fnAlgoManager();
+
     QApplication a(argc, argv);
     ImageWizard wizard;
    

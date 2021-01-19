dca2c15b2c3f6b6fd6ccbaed6b2ee8348c08ff92
TaylorAtkins <43218109+TaylorAtkins@users.noreply.github.com>
2021-01-19 14:38:22 -0800

    Removed precompiled headers & linked the AlgoManager Library to the Unit Tests project as a reference.

--- UnitTests/test.cpp
+++ UnitTests/test.cpp
@@ -1,6 +1,10 @@
-#include "pch.h"
+#include "gtest/gtest.h"
+#include "../AlgoManager/algomanager.h"
 
-TEST(TestCaseName, TestName) {
+//This is just an example test. It should be deleted once actual tests are added
+TEST(TestSuiteName, TestName) {
+  AlgoManager::AlgoManager test;
+  test.testFunction();
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }

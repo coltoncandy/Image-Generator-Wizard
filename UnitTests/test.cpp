c70bd077d7be7075d1f68a5b05d831238024ab8a
TaylorAtkins <43218109+TaylorAtkins@users.noreply.github.com>
2021-01-19 22:30:21 -0800

    Adding second test.

--- UnitTests/test.cpp
+++ UnitTests/test.cpp
@@ -4,7 +4,11 @@
 //This is just an example test. It should be deleted once actual tests are added
 TEST(TestSuiteName, TestName) {
   AlgoManager::AlgoManager test;
-  test.testFunction();
+  //test.testFunction();
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+TEST(TestSuiteName, SecondTest) {
+	EXPECT_EQ(2, 2);
+	EXPECT_TRUE(true);
 }

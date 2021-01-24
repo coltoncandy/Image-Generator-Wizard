d0d1019624acfd6021a6c3430f41d0df7a297319
Topher Maxwell <56896619+cmaxwell2@users.noreply.github.com>
2021-01-24 15:23:05 -0800

    Changed AlgoManager to blur the initial image before overlaying it onto the background.

--- AlgoManager/algomanager.cpp
+++ AlgoManager/algomanager.cpp
@@ -11,9 +11,11 @@ namespace AlgoManager {
         return;
     }
     void AlgoManager::overlayWrapper(const std::string& bg, const std::string& fg) {
-        Mat res2 = imread(fg, IMREAD_UNCHANGED);          
+        Mat res2 = imread(fg, IMREAD_UNCHANGED);
+        Mat blurredImg = blurEdges(res2, 9); //integer argument is for side length of grid (so 3 is 3x3 grid centered on pixels). please only use odd numbers.
         Mat background = imread(bg, IMREAD_COLOR);
-        Mat res3 = overlay(background, res2, Point(0, 0));
+        //Mat res3 = overlay(background, res2, Point(0, 0));
+        Mat res3 = overlay(background, blurredImg, Point(0, 0));
 
         imshow("res3", res3);
         waitKey(0);

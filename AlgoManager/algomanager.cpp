27b250369ff5a9261f8e68ff674cfd7a2f0f32c3
TJ Nuccio <tj_nuccio@yahoo.com>
2021-01-24 12:56:34 -0800

    Updated imwrite to write processed target to the path passed as parameter

--- AlgoManager/algomanager.cpp
+++ AlgoManager/algomanager.cpp
@@ -6,13 +6,12 @@ namespace AlgoManager {
     void AlgoManager::grabCutWrapper(const std::string& path) {
 
         Mat res = grabCut(path);
-        //Result still needs to be written to disk or saved as a variable, but waiting on further direction on this before uncommenting the line below
-        imwrite("result.png", res);
+        imwrite(path, res);             //Write processed target back to target's path  
 
         return;
     }
     void AlgoManager::overlayWrapper(const std::string& bg, const std::string& fg) {
-        Mat res2 = imread("result.png", IMREAD_UNCHANGED);
+        Mat res2 = imread(fg, IMREAD_UNCHANGED);          
         Mat background = imread(bg, IMREAD_COLOR);
         Mat res3 = overlay(background, res2, Point(0, 0));
 

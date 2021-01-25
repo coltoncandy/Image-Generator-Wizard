18c226efb0b73d16562f60e43cacf14b40b8517f
TaylorAtkins <43218109+TaylorAtkins@users.noreply.github.com>
2021-01-24 18:35:49 -0800

    updated algo function called in algomanager_test to new name.

--- UnitTests/algomanager_tests.cpp
+++ UnitTests/algomanager_tests.cpp
@@ -4,7 +4,7 @@
 TEST(AlgoManagerTests, TestThatIfAnInvalidPathIsGivenToGrabCutAnErrorMessageIsDisplayed) {
 	AlgoManager::AlgoManager testManager;
 	testing::internal::CaptureStdout();
-	testManager.grabCut("C:/Non/Existant/Path");
+	testManager.grabCutWrapper("C:/Non/Existant/Path");
 	std::string stdOutput = testing::internal::GetCapturedStdout();
 	EXPECT_EQ("\n Could not read file path\n" , stdOutput);
 }

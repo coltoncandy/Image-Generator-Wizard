60ef50d9fd1045c39150cccff7d9d797f23624f0
TJ Nuccio <tj_nuccio@yahoo.com>
2021-01-31 13:13:49 -0800

    Added opencv headers and prototype for rotate function

--- AlgoManager/transforms.h
+++ AlgoManager/transforms.h
@@ -1,6 +1,8 @@
 #include "opencv2/imgproc.hpp"
-using namespace cv; 
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+using namespace cv;
 
 //Overlay foreground on background image at given coordinate 
 Mat overlay(Mat background, Mat foreground, Point location);
-
+Mat rotate(Mat target);

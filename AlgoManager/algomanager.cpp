a1f1c7651c4701e1eb9bc8c0d082eb51c668bce8
Topher Maxwell <56896619+cmaxwell2@users.noreply.github.com>
2021-02-01 09:17:22 -0800

    AlgoManager now uses the Gaussian Blur after grabCut, instead of the transparency blur.

--- AlgoManager/algomanager.cpp
+++ AlgoManager/algomanager.cpp
@@ -12,10 +12,12 @@ namespace AlgoManager {
     }
     void AlgoManager::overlayWrapper(const std::string& bg, const std::string& fg) {
         Mat res2 = imread(fg, IMREAD_UNCHANGED);
-        Mat blurredImg = blurEdges(res2, 9); //integer argument is for side length of grid (so 3 is 3x3 grid centered on pixels). please only use odd numbers.
+        Mat blurredImg1 = blurEdgesGaussian(res2, 7, 3);//first integer argument is for side length of grid (so 5 is 5x5 grid centered on pixels). please only use 3, 5, or 7. widthToBlur = how far away from transparent pixels will be blurred.
+        //imshow("blurredImg1", blurredImg1);
+        Mat blurredImg2 = blurEdgesTransparency(res2, 9); //integer argument is for side length of grid (so 3 is 3x3 grid centered on pixels). please only use odd numbers.
         Mat background = imread(bg, IMREAD_COLOR);
         //Mat res3 = overlay(background, res2, Point(0, 0));
-        Mat res3 = overlay(background, blurredImg, Point(0, 0));
+        Mat res3 = overlay(background, blurredImg1, Point(0, 0));
 
         imshow("res3", res3);
         waitKey(0);

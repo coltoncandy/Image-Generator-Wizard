22f280ecb9f22246bce80004d74342f057b2183c
TJ Nuccio <tj_nuccio@yahoo.com>
2021-02-01 20:00:15 -0800

    Fixed prototype for rotation

--- AlgoManager/transforms.h
+++ AlgoManager/transforms.h
@@ -6,6 +6,6 @@ using namespace cv;
 
 //Overlay foreground on background image at given coordinate 
 Mat overlay(Mat background, Mat foreground, Point location);
-Mat rotation(Mat target);
+Mat rotation(Mat target, int angleBounds);
 Mat cropBackground(Mat background, Point origin, Point terminal, int minWidth, int minHeight);
 

77e55ff22fef134ae8d26a0e8e5d501282157e9c
Colton <colton6@pdx.edu>
2021-02-03 08:22:49 -0800

    started adding instructions

--- ImageWizard/backgroundremoval.cpp
+++ ImageWizard/backgroundremoval.cpp
@@ -6,6 +6,8 @@ BackgroundRemoval::BackgroundRemoval(const QString& title, QWidget* parent)
 	ui.setupUi(this);
 	QLabel* titleLabel = findChild<QLabel*>("title");
 	titleLabel->setText(title);
+	QLabel* instructions = findChild<QLabel*>("instructions");
+	instructions->setText("<b>TJs List</b><br><ul><li>1. Do something with N</li><li>2. Do something with ESC</li></ul>");
 }
 
 BackgroundRemoval::~BackgroundRemoval() {

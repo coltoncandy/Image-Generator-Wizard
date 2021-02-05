8bead7515b56888d9e9feaf26d475586ddaf65db
TaylorAtkins <43218109+TaylorAtkins@users.noreply.github.com>
2021-02-04 18:37:29 -0800

    Wrote tests for filemanager & fixed a few bugs in filemanager

--- AlgoManager/filemanager.cpp
+++ AlgoManager/filemanager.cpp
@@ -1,8 +1,10 @@
 #include "fileManager.h"
 
 void getRandomBackgrounds(int backgroundNum, std::string absolutePath, std::string *& backgroundList) {
-	if(backgroundNum < 1)
-		throw "The number of background selected to return must be greater than 0.";
+	if(backgroundNum < 1) {
+		std::string errorMessage = "The number of background selected to return must be greater than 0.";
+		throw errorMessage;
+	}
 
 	if(backgroundList) {
 		delete[] backgroundList;
@@ -16,7 +18,9 @@ void getRandomBackgrounds(int backgroundNum, std::string absolutePath, std::stri
 
     HANDLE hFind = FindFirstFileA(searchPath.c_str() , &data);
     if(hFind == INVALID_HANDLE_VALUE) {
-		throw "Could not find given directory";
+		std::string errorMessage = "Could not find given directory: ";
+		errorMessage.append(absolutePath);
+		throw errorMessage;
     }
 
 	std::vector<std::string> fileList;
@@ -32,6 +36,12 @@ void getRandomBackgrounds(int backgroundNum, std::string absolutePath, std::stri
 	} while(FindNextFileA(hFind, &data));
 	FindClose(hFind);
 
+
+	if(fileList.size() < 1) {
+		std::string errorMessage = "No .png files could be found in the given directory: ";
+		errorMessage.append(absolutePath);
+		throw errorMessage;
+	}
 
 	backgroundList = new std::string[backgroundNum];
 	int totalBackgrounds = fileList.size();

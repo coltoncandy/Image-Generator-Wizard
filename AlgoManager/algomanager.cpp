4441068acb74b69d124b71b2377e5c93c46d1a42
TJ Nuccio <tj_nuccio@yahoo.com>
2021-02-07 11:12:27 -0800

    Started on process function to manage applying transforms randomly to processed target

--- AlgoManager/algomanager.cpp
+++ AlgoManager/algomanager.cpp
@@ -6,34 +6,57 @@
 namespace AlgoManager {
     void AlgoManager::grabCutWrapper(const std::string& path) {
 
-        Mat res = grabCut(path);
-        imwrite(path, res);             //Write processed target back to target's path  
+        if(path.empty())
+            return; 
+
+        Mat processedTarget = grabCut(path);
+        imwrite(path, processedTarget);             //Write processed target back to target's path  
+
+        process(path); 
 
         return;
     }
     //Parameters: Path to target, number of processed images specified by user (?)
-    void process() {
-        //Random number of transform calls
-        //Random parameters
-        //Random order? 
-        //int numOfCalls = rand() % 5; 
+    void AlgoManager::process(const std::string& targetPath) {
 
-        //for(int i = 0; i < numOfCalls; i++) {
-        //    //Flip
-        //    //Rotate
-        //    //Dilate / enlarge 
-        //    //Noise injection 
-        //}
+        Mat target = imread(targetPath);
 
-        // crop
+        int angleBounds; 
+        int flipCode; 
+        int choice; 
+
+        int numOfCalls = (rand() % 10) + 1;
+
+        for(int i = 0; i < numOfCalls; i++) {
+            
+            choice = rand() % 2; 
+            
+            switch(choice) {
+                case 0: 
+                    angleBounds = rand() % 26;                          //Random angleBounds between 0 - 25 degrees 
+                    target = rotation(target, angleBounds);             //Rotation will occur within the bounds of -angleBounds to +angleBounds degrees
+                case 1: 
+                    flipCode = (rand() % 2) - 1;                        //Random flipCode between -1 and 1  
+                    target = flipIt(target, flipCode); 
+            }
+        }
+
+        namedWindow("display", WINDOW_NORMAL);                          //Displays target after running through transformations 
+        imshow("display", target); 
+        waitKey(0); 
+
     }
     void AlgoManager::overlayWrapper(const std::string& bg, const std::string& fg) {
+
+        if(bg.empty() || fg.empty())
+            return; 
+
         Mat foreground = imread(fg, IMREAD_UNCHANGED);          
         Mat background = imread(bg, IMREAD_COLOR);
         Mat blended = overlay(background, foreground, Point(0, 0));
 
-        imshow("blended", blended);
-        waitKey(0);
+        /*imshow("blended", blended);
+        waitKey(0);*/
 
         return; 
     }
--- AlgoManager/algomanager.h
+++ AlgoManager/algomanager.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <string>
 
 namespace AlgoManager {
@@ -7,7 +6,6 @@ namespace AlgoManager {
 	public:
 		static void grabCutWrapper(const std::string& path);
 		static void overlayWrapper(const std::string& bg, const std::string& fg); 
-		void process(); 
+		static void process(const std::string& targetPath); 
 	};
 }
-

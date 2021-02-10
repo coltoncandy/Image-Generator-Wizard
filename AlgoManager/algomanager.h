b4666448c69225c693821b8ef9f885c1b54a8f98
TJ Nuccio <tj_nuccio@yahoo.com>
2021-02-10 15:41:39 -0800

    Updated process to take paths to all images

--- AlgoManager/algomanager.h
+++ AlgoManager/algomanager.h
@@ -6,6 +6,6 @@ namespace AlgoManager {
 	public:
 		static void grabCutWrapper(const std::string& path);
 		static void overlayWrapper(const std::string& bg, const std::string& fg); 
-		static void process(const std::string& targetPath); 
+		static void process(const std::string& initialPath, const std::string& targetPath, const std::string& backgroundPath, const std::string& destinationPath);
 	};
 }

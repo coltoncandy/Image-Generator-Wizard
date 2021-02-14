dddff4b133d8020fd8e398c8375660e628295ace
Tula <tholt@cecs.pdx.edu>
2021-02-13 18:57:15 -0800

    Added reset icon and styling. Changed styling of please wait prompt in processing window. Changed cursor to busy wait to load in processing window.

--- ImageWizard/selectdestination.cpp
+++ ImageWizard/selectdestination.cpp
@@ -8,13 +8,23 @@
 #include <QMimeData>
 #include <QDropEvent>
 
+
 SelectDestination::SelectDestination(const QString& title, QString* destination, QWidget* parent) : WizardPage(parent) {
 	QObject::connect(&chooser, &QFileDialog::directoryEntered, this, &SelectDestination::setDirectory);
 	ui.setupUi(this);
 	chooser.setFileMode(QFileDialog::FileMode::Directory);
 
-	//resetButton = findChild<QPushButton*>("reset");
-	//QObject::connect(resetButton, &QPushButton::released, this, &TargetSelector::reset);
+	resetButton = findChild<QPushButton*>("reset");
+	resetButton->setIcon(QIcon("  "));
+	resetButton->setStyleSheet("border-left: 10px transparent; border-right: 10px transparent;""border-top: 3px transparent; border-bottom: 3px transparent;"); // remove edges of button
+	resetButton->setIconSize(QSize(100, 50));
+	QObject::connect(resetButton, &QPushButton::released, this, &SelectDestination::reset);
+	resetButton->setCursor(QCursor(Qt::PointingHandCursor));
+	// Set reset button icon and hover icon
+	QString reset = QDir::homePath() + "/source/repos/image-generator/icons/reset.png";
+	QString styleSheet = "QPushButton#reset{ image: url(%1); background-repeat: no-repeat; } QPushButton:hover#reset{ image: url(%2); background-repeat: no-repeat; }";
+	QString resetHover = QDir::homePath() + "/source/repos/image-generator/icons/resetHover.png";
+	setStyleSheet(styleSheet.arg(reset).arg(resetHover));
 
 	QLabel* titleLabel = findChild<QLabel*>("title"); // show title on Qwidget
 	titleLabel->setText(title);

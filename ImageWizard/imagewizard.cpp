22a21d9666dccd747ebef4420ab4f7defc496434
Colton <colton6@pdx.edu>
2021-02-15 02:52:25 -0800

    removed unnecessary checks in goNext function

--- ImageWizard/imagewizard.cpp
+++ ImageWizard/imagewizard.cpp
@@ -106,16 +106,6 @@ void ImageWizard::goNext() {
 		AlgoManager::AlgoManager::grabCutWrapper(target->path->toStdString());		//NOTE: Needs to be changed to target->path after SC-35 is complete 
 		target->image->load(*target->path);											//Update target struct for processed image written to target->path 
 	}
-	else if(frames->currentWidget() == backgroundChooser) { //background image upload page
-		if(!background->loaded) {
-			return;
-		}
-	}
-	else if(frames->currentWidget() == selectDestination) { //background image upload page
-		if(!selectDestination->isReady()) {
-			return;
-		}
-	}
 	else if(frames->currentWidget() == processingWindow) {
 		AlgoManager::AlgoManager::overlayWrapper(background->path->toStdString(), target->path->toStdString());		//Send image containing target to grabCut
 

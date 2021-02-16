df464a88093f987709bf8a2fcf39fee651798cf0
Yichao <yichao@pdx.edu>
2021-02-15 19:13:07 -0800

    Align the text of instruction

--- ImageWizard/backgroundremoval.cpp
+++ ImageWizard/backgroundremoval.cpp
@@ -8,7 +8,8 @@ BackgroundRemoval::BackgroundRemoval(const QString& title, QWidget* parent)
 	titleLabel->setText(title);
 	QLabel* instructions = findChild<QLabel*>("instructions");
 	instructions->setStyleSheet("QLabel { color : white; }");
-	instructions->setText("<b>Instructions: </b><br><ul><li>1. Hold 'ctrl' key and use mouse to draw on areas of background</li><li>3. Hold 'shift' key and use mouse to draw on areas of foreground</li><li>4. Press 'n' to run one iteration of GrabCut</li><li>5. Press 'r' key to start over </li><li>6. Press 'esc' key to continue once satisifed with result</li></ul>");
+	instructions->setText("<ul>Instructions: <br><li>1. Hold 'ctrl' key and use mouse to draw on areas of background</li><li>3. Hold 'shift' key and use mouse to draw on areas of foreground</li><li>4. Press 'n' to run one iteration of GrabCut</li><li>5. Press 'r' key to start over </li><li>6. Press 'esc' key to continue once satisifed with result</li></ul>");
+	
 }
 
 BackgroundRemoval::~BackgroundRemoval() {

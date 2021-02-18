c0ce12197cbd1f01bbd0851344dce66f4c6c9484
Yichao <yichao@pdx.edu>
2021-02-18 05:02:33 -0800

    Fixed  instruction text align

--- ImageWizard/backgroundremoval.cpp
+++ ImageWizard/backgroundremoval.cpp
@@ -10,7 +10,7 @@ BackgroundRemoval::BackgroundRemoval(const QString& title, ImageInfo* target, QW
 	titleLabel->setText(title);
 	QLabel* instructions = findChild<QLabel*>("instructions");
 	instructions->setStyleSheet("QLabel { color : white; }");
-	instructions->setText("<ul><b>Instructions: </b><br><li>1. Hold 'ctrl' key and use mouse to draw on areas of background</li><li>3. Hold 'shift' key and use mouse to draw on areas of foreground</li><li>4. Press 'n' to run one iteration of GrabCut</li><li>5. Press 'r' key to start over </li><li>6. Press 'esc' key to continue once satisifed with result</li></ul>");
+	instructions->setText("<li><b>Instructions: </b></li><br><li>1. Hold 'ctrl' key and use mouse to draw on areas of background</li><li>2. Hold 'shift' key and use mouse to draw on areas of foreground</li><li>3. Press 'n' to run one iteration of GrabCut</li><li>4. Press 'r' key to start over </li><li>5. Press 'esc' key to continue once satisifed with result</li>");
 	cropButton = findChild<QPushButton*>("cropButton");
 	targetImage = target;
 	enableDisplay = false;

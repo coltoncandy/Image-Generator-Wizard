12560e6202de6a87d68f0485a1ab473fdaa343b6
Yichao <yichao@pdx.edu>
2021-02-23 17:49:18 -0800

    Change it back sigh, cause TJ already fixed it

--- AlgoManager/algomanager.h
+++ AlgoManager/algomanager.h
@@ -8,7 +8,7 @@ namespace AlgoManager {
 	public:
 		static bool grabCutWrapper(const std::string& path);
 		static void overlayWrapper(const std::string& bg, const std::string& fg); 
-		static cv::Mat process(const std::string& initialPath, const std::string& targetPath, const std::string& backgroundPath, int randomNumber);
+		static cv::Mat process(const std::string& initialPath, const std::string& targetPath, const std::string& backgroundPath);
 		static void batchProcess(int imageNum, const std::string& initialPath, const std::string& targetPath,  std::string* backgroundPaths, std::vector<cv::Mat>& imageBatch);
 	};
 }

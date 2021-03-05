f3e491be0c6e4e13b27588c1fb8a1f2ad0b266ca
Tula <tholt@cecs.pdx.edu>
2021-03-04 18:37:30 -0800

    Added Titles to batch and single image choice

--- ImageWizard/batchchoice.cpp
+++ ImageWizard/batchchoice.cpp
@@ -12,13 +12,12 @@ BatchChoice::BatchChoice(QWidget* parent) : WizardPage(parent) {
 	singleBtn->setCursor(QCursor(Qt::PointingHandCursor));
 	QString imageHover = QDir::currentPath() + "/icons/imageHover.png";
 	QString image = QDir::currentPath() + "/icons/image.png";
-	QString single = QDir::currentPath() + "/icons/single.png";
-	QString singleStyleSheet = "QPushButton#singleBtn{ image: url(" + image + "); width: 300px; height: 220px; background-repeat: no-repeat; border-left: 10px transparent; border-right: 10px transparent; border-top: 3px transparent; border-bottom: 3px transparent; } QPushButton:hover#singleBtn{ image: url(" + imageHover + "); background-repeat: no-repeat; }";
+	QString singleStyleSheet = "QPushButton#singleBtn{ font: 14pt Microsoft Sans Serif; color:rgb(250, 250, 250); image: url(" + image + "); width: 300px; height: 220px; background-repeat: no-repeat; border-left: 10px transparent; border-right: 10px transparent; border-top: 3px transparent; border-bottom: 3px transparent; } QPushButton:hover#singleBtn{ image: url(" + imageHover + "); background-repeat: no-repeat; }";
 	QString bimageHover = QDir::currentPath() + "/icons/batchImageHover.png";
 	QString bimage = QDir::currentPath() + "/icons/batchImage.png";
-	QString batch = QDir::currentPath() + "/icons/batch.png";
-	QString batchStyleSheet = "QPushButton#batchBtn{ image: url(" + bimage + "); width: 220px; height: 150px; background-repeat: no-repeat; border-left: 10px transparent; border-right: 10px transparent; border-top: 3px transparent; border-bottom: 3px transparent; } QPushButton:hover#batchBtn{ image: url(" + bimageHover + "); background-repeat: no-repeat; }";
+	QString batchStyleSheet = "QPushButton#batchBtn{ font: 14pt Microsoft Sans Serif; color:rgb(250, 250, 250); image: url(" + bimage + "); width: 220px; height: 160px; background-repeat: no-repeat; border-left: 10px transparent; border-right: 10px transparent; border-top: 3px transparent; border-bottom: 3px transparent; } QPushButton:hover#batchBtn{ image: url(" + bimageHover + "); background-repeat: no-repeat; }";
 
+	
 	batchBtn->setToolTip("Batch");
 	singleBtn->setToolTip("Single");
 	batchBtn->setStyleSheet(batchStyleSheet);
